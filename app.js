fb9d6d2d0649d8a350f31adcd8afbddd7df2ec43
MEEZAN <harismalik1@pm.me>
2023-03-05 19:08:54 +0530

    second commit

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const searchUser = document.getElementById("searchUser");
 
 searchUser.addEventListener("keyup", (e) => {
   const userText = e.target.value;
+  if (userText === "") {
+    footerStyle(false);
+  } else {
+    footerStyle(true);
+  }
   if (userText !== "") {
     // make http call
     github.getUser(userText).then((data) => {
       if (data.profile.message === "Not Found") {
-        ui.showAlert("User not found",'alert alert-danger');
-    } else {
+        ui.showAlert("User not found", "alert alert-danger");
+      } else {
         ui.showProfile(data.profile);
         ui.showRepos(data.repos);
       }
@@ -18,5 +23,15 @@ searchUser.addEventListener("keyup", (e) => {
   } else {
     // clear profile
     ui.clearProfile();
-}
+  }
 });
+
+const footer = document.querySelector("footer");
+
+function footerStyle(val) {
+  if (val) {
+    footer.style.display = "none";
+  } else {
+    footer.style.display = "block";
+  }
+}
